9fc546ad4d449c7504e71863008d09c5198ad8e1
Daniel Wen <daniel.wen@ericsson.com>
2016-07-09 22:09:33 +0800

    Dao+Service+Web completed

---
+++ src/main/java/org/seckill/exception/SeckillCloseException.java
@@ -0,0 +1,20 @@
+package org.seckill.exception;
+
+/**
+ * √Î…±πÿ±’“Ï≥£
+ * @author ewendia
+ */
+@SuppressWarnings("serial")
+public class SeckillCloseException extends SeckillException {
+
+	public SeckillCloseException(String message) {
+		super(message);		
+	}
+
+	public SeckillCloseException(String message, Throwable cause) {
+		super(message, cause);		
+	}
+	
+	
+
+}
